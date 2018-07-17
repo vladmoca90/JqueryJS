f40724b8bf011e6eef6e05b7c55541cd1d57811c
Vlad Mocanu <vladmoca90@gmail.com>
2018-07-17 22:23:05 +0100

    The code was fixed and it works

--- scripts/JQuery/truncate-text.js
+++ scripts/JQuery/truncate-text.js
@@ -1,20 +1,26 @@
-$(function() {
+$(function () {
 
-    function truncateText(text, n) {
+    $('button').click(function () {
 
         var text = $('#textContent').val();
         var n = $('#numberValue').val();
 
-        if(text.length == 0) {
-            throw new Error('The input for text cannot be empty');
-        }
-        if(n == 0) {
-            return text;
-        }
-        if(n < 0) {
-            throw new Error('n must be greater or equal to 0');
-        }
-    }
+        function truncateText(text, n) {
 
-    truncateText(text, n);
+            if (text.length == 0) {
+                throw new Error('The text cannot be empty');
+            }
+            if (n <= 0) {
+                throw new Error('n must be greater or equal to 1');
+            }
+
+            var splitText = text.trim().split('');
+
+            var truncatedText = splitText.splice(0, n).join('');
+
+            $('p').text(truncatedText);
+        }
+
+        truncateText(text, n);
+    });
 });
