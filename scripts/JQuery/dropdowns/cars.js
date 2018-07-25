fc76128c06dd89c00e47fc04935a2ebd2c54e3fe
Vlad Mocanu <vladmoca90@gmail.com>
2018-07-25 22:37:48 +0100

    Will create another dropdown with car models and makes

---
+++ scripts/JQuery/dropdowns/cars.js
@@ -0,0 +1,21 @@
+var cars = [{
+        model: 'Mercedes-Benz',
+        make: 'E50',
+    },
+    {
+        model: 'Honda',
+        make: 'Accord',
+    },
+    {
+        model: 'Ford',
+        make: 'F250',
+    },
+    {
+        model: 'Toyota',
+        make: 'RAV4',
+    },
+]
+
+$(function () {
+
+});
