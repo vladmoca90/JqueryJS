f368781867f01aa7020d16ccb2d30efc59a9ab5d
Vlad Mocanu <vladmoca90@gmail.com>
2018-07-29 11:46:07 +0100

    Populated the first dropdown

--- scripts/JQuery/dropdowns/cars.js
+++ scripts/JQuery/dropdowns/cars.js
@@ -14,8 +14,40 @@ var cars = [{
         model: 'Toyota',
         make: 'RAV4',
     },
-]
+];
+
+var stocks = [3, 1, 4, 1];
 
 $(function () {
+    function addModelsToDrop(cars, dropId) {
+        if (cars.length == 0) {
+            throw new Error('The cars list ust be given');
+        }
 
+        $(dropId).empty();
+
+        var blank = $('<option></option>');
+
+        $(dropId).append(blank);
+
+        for (var i = 0; i < cars.length; i++) {
+            var option = $('<option></option>');
+
+            option.val(cars[i].model);
+            option.html(cars[i].model);
+
+            $(dropId).append(option);
+        }
+    }
+
+    addModelsToDrop(cars, '#modelDrop');
+
+    $('#modelDrop').change(function () {
+        var selectedOption = $(this).find(':selected');
+        var carCode = ;
+
+        for (var i = 0; i < cars.length; i++) {
+
+        }
+    });
 });
