100afb635b60f6d9f682792c47d70189fdd5a65b
Vlad Mocanu <vladmoca90@gmail.com>
2018-07-31 23:16:29 +0100

    Added new changes to the bar chart

--- scripts/Graps-and-charts/oil-reserves.js
+++ scripts/Graps-and-charts/oil-reserves.js
@@ -1,105 +1,104 @@
 window.onload = function () {
 
-    var chart = new CanvasJS.Chart("bar", {
+    var chart = new CanvasJS.Chart('bar', {
         animationEnabled: true,
+        dataPointWidth: 30,
         title: {
-            text: "Crude Oil Reserves vs Production, 2016"
+            fontSize: 21,
+            fontWeight: 'lighter',
+            padding: 20,
+            text: 'Crude Oil Reserves vs Production, 2016',
+        },
+        axisX: {
+            labelFontSize: 14,
         },
         axisY: {
-            title: "Billions of Barrels",
-            titleFontColor: "#4F81BC",
-            lineColor: "#4F81BC",
-            labelFontColor: "#4F81BC",
-            tickColor: "#4F81BC"
+            gridColor: '#ddd',
+            title: 'Billions of Barrels',
+            titleFontColor: '#4F81BC',
+            titleFontSize: 16,
+            lineColor: '#4F81BC',
+            labelFontColor: '#4F81BC',
+            labelFontSize: 12,
+            tickColor: '#4F81BC'
         },
         axisY2: {
-            title: "Millions of Barrels/day",
-            titleFontColor: "#C0504E",
-            lineColor: "#C0504E",
-            labelFontColor: "#C0504E",
-            tickColor: "#C0504E"
+            title: 'Millions of Barrels/day',
+            titleFontColor: '#C0504E',
+            titleFontSize: 16,
+            lineColor: '#C0504E',
+            labelFontColor: '#C0504E',
+            labelFontSize: 12,
+            tickColor: '#C0504E'
         },
         toolTip: {
             shared: true
         },
-        legend: {
-            cursor: "pointer",
-            itemclick: toggleDataSeries
-        },
         data: [{
-                type: "column",
-                name: "Proven Oil Reserves (bn)",
-                legendText: "Proven Oil Reserves",
+                type: 'column',
+                name: 'Proven Oil Reserves (bn)',
+                legendText: 'Proven Oil Reserves',
                 showInLegend: true,
                 dataPoints: [{
-                        label: "Saudi",
+                        label: 'Saudi Arabia',
                         y: 266.21
                     },
                     {
-                        label: "Venezuela",
+                        label: 'Venezuela',
                         y: 302.25
                     },
                     {
-                        label: "Iran",
+                        label: 'Iran',
                         y: 157.20
                     },
                     {
-                        label: "Iraq",
+                        label: 'Iraq',
                         y: 148.77
                     },
                     {
-                        label: "Kuwait",
+                        label: 'Kuwait',
                         y: 101.50
                     },
                     {
-                        label: "UAE",
+                        label: 'UAE',
                         y: 97.8
                     }
                 ]
             },
             {
-                type: "column",
-                name: "Oil Production (million/day)",
-                legendText: "Oil Production",
-                axisYType: "secondary",
+                type: 'column',
+                name: 'Oil Production (million/day)',
+                legendText: 'Oil Production',
+                axisYType: 'secondary',
                 showInLegend: true,
                 dataPoints: [{
-                        label: "Saudi",
+                        label: 'Saudi Arabia',
                         y: 10.46
                     },
                     {
-                        label: "Venezuela",
+                        label: 'Venezuela',
                         y: 2.27
                     },
                     {
-                        label: "Iran",
+                        label: 'Iran',
                         y: 3.99
                     },
                     {
-                        label: "Iraq",
+                        label: 'Iraq',
                         y: 4.45
                     },
                     {
-                        label: "Kuwait",
+                        label: 'Kuwait',
                         y: 2.92
                     },
                     {
-                        label: "UAE",
+                        label: 'UAE',
                         y: 3.1
                     }
                 ]
             }
         ]
     });
+
     chart.render();
-
-    function toggleDataSeries(e) {
-        if (typeof (e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
-            e.dataSeries.visible = false;
-        } else {
-            e.dataSeries.visible = true;
-        }
-        chart.render();
-    }
-
 }
