286fb5f8ab5f9a6aab0fda68fd2c2c055f732615
Vlad Mocanu <vladmoca90@gmail.com>
2018-08-13 22:34:43 +0100

    The number of characters is displayed

--- scripts/JQuery/twitter.js
+++ scripts/JQuery/twitter.js
@@ -2,15 +2,34 @@ $(function () {
     var maxCount = 140;
     var maxCharacters = $('<span id="maxCharacters"></span>');
 
-    $('#commentSection').change(function() {
-
+    $('#commentSection').change(function () {
+        if ($(this).val().length > 0 && $(this).val().length <= 40) {
+            $('#counter').removeClass('negative');
+            $('#counter').addClass('high');
+            $('#counter').html(maxCount - $(this).val().length);
+        }
+        if ($(this).val().length >= 41 && $(this).val().length <= 100) {
+            $('#counter').removeClass('high');
+            $('#counter').addClass('medium');
+            $('#counter').html(maxCount - $(this).val().length);
+        }
+        if ($(this).val().length >= 101 && $(this).val().length <= 140) {
+            $('#counter').removeClass('medium');
+            $('#counter').addClass('low');
+            $('#counter').html(maxCount - $(this).val().length);
+        }
+        if ($(this).val().length > 140) {
+            $('#counter').removeClass('low');
+            $('#counter').addClass('negative');
+            $('#counter').html(maxCount - $(this).val().length);
+        }
     });
 
     $('button').click(function () {
         var content = $('#commentSection').val();
         var tweet = $('<li class="text"></li>');
 
-        if (content.length == 0 || content.length > 140 ) {
+        if (content.length == 0 || content.length > 140) {
             return;
         }
 
