4b190311db64ba093c6e4a8e6e99878dbf8bfc7d
Vlad Mocanu <vladmoca90@gmail.com>
2018-08-13 21:16:15 +0100

    Adding comment character limits

--- scripts/JQuery/twitter.js
+++ scripts/JQuery/twitter.js
@@ -1,12 +1,23 @@
 $(function () {
+    var maxCount = 140;
+    var maxCharacters = $('<span id="maxCharacters"></span>');
+
+
+    // $('#characters').append(maxCharacters.html(160));
+
+    // if (commentWritten.length > 0) {
+    //     $('#characters').append(maxCharacters.html(160) - commentWritten.length);
+    // }
 
     $('button').click(function () {
+        var content = $('#commentSection').val();
+        var tweet = $('<li class="text"></li>');
 
-        var commentWritten = $('#commentSection').val();
-        var commentShown = $('<li class="text"></li>');
+        if (content.length == 0 || content.length > 140 ) {
+            return;
+        }
 
-        commentShown.val(commentWritten);
-        commentShown.html(commentWritten);
-        $('#commentDisplay').append(commentShown);
+        tweet.html(content);
+        $('#commentDisplay').prepend(tweet);
     });
 });
