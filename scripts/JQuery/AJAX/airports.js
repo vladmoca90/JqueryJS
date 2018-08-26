322fe73c7c9ee58692f4ef2a7a944c03d69030c8
Vlad Mocanu <vladmoca90@gmail.com>
2018-08-26 20:59:06 +0100

    Added a gif image

--- scripts/JQuery/AJAX/airports.js
+++ scripts/JQuery/AJAX/airports.js
@@ -2,23 +2,14 @@ $(function() {
 
     var airportsList = [];
 
-    function getAirports(airportsList) {
-        if(airportsList.length == 0) {
-            
-            throw new Error('The airports list must be given');
-        }
-
-        for(var i = 0; i < airportsList.length; i++) {
-            airportsList.push(airports);
-        }
-    }
-
     var airportsUrl = 'http://sampleapis20180514091454.azurewebsites.net/api/Airports';
 
     $.ajax(airportsUrl, {
         method: 'GET',
         success: function(response) {
-            getAirports(response.airportsList);
+            console.log(response);
+
+            airportsList = response.airports;
         },
         error: function (err) {
             console.err(err);
