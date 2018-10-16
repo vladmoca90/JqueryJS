289a5d593fccb7f56d322e89bfee7f5dcfb9d0a1
Vlad Mocanu <vladmoca90@gmail.com>
2018-10-16 22:46:02 +0100

    Added a new url

--- scripts/JQuery/AJAX/airports.js
+++ scripts/JQuery/AJAX/airports.js
@@ -1,8 +1,6 @@
 $(function() {
 
-    var airportsList = [];
-
-    var airportsUrl = 'http://sampleapis20180514091454.azurewebsites.net/api/Airports';
+    var airportsUrl = 'https://gist.githubusercontent.com/tdreyno/4278655/raw/7b0762c09b519f40397e4c3e100b097d861f5588/airports.json';
 
     $.ajax(airportsUrl, {
         method: 'GET',
