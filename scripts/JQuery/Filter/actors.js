1791c400901a1ff5a9d0cb6b56be60f61bb79de0
Vlad Mocanu <vladmoca90@gmail.com>
2018-10-21 22:20:54 +0100

    Working to create a list

---
+++ scripts/JQuery/Filter/actors.js
@@ -0,0 +1,53 @@
+var names = [{
+        name: "Vlad Mocanu",
+    },
+    {
+        name: "Alexandru Mocanu",
+    },
+    {
+        name: "Rebecca Docherty",
+    },
+    {
+        name: "Leon Williams",
+    },
+    {
+        name: "Klaudia Rudna",
+    },
+    {
+        name: "Libo Bian",
+    },
+];
+
+$(function () {
+
+    function addNamesToDropdown(names) {
+        if (names.length == 0) {
+            throw new Error('The list with names must be provided');
+        }
+
+        for (var i = 0; i < names.length; i++) {
+            var option = $('<option></option>');
+
+            $('#names').append(option);
+            $('option:nth-of-type(1)').attr('disabled', 'disabled');
+            option.html(names[i].name);
+        }
+    }
+
+    addNamesToDropdown(names);
+
+    function addNamesToList(names) {
+         if (names.length == 0) {
+            throw new Error('The list with names must be provided');
+        }
+
+        for (var i = 0; i < names.length; i++) {
+            var nameItem = $('<li></li>');
+
+            $('.namesList').append(nameItem);
+            nameItem.html(names[i].name);
+        }
+    }
+
+    addNamesToList(names);
+});
