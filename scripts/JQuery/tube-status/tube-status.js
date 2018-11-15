641bd5d47c83bdfdad07223659c1c7ce441571ab
Vlad Mocanu <vladmoca90@gmail.com>
2018-11-15 21:53:27 +0000

    Deleted the code

--- scripts/JQuery/tube-status/tube-status.js
+++ scripts/JQuery/tube-status/tube-status.js
@@ -2,62 +2,77 @@ var data = {
     lines: [{
             name: 'Bakerloo',
             status: 'Good service',
+            id: 1,
         },
         {
             name: 'Central',
             status: 'Good service',
+            id: 1,
         },
         {
             name: 'Circle',
             status: 'Good service',
+            id: 1,
         },
         {
             name: 'District',
             status: 'Partially closed',
+            id: 2,
         },
         {
             name: 'Hammersmith & city',
             status: 'Partially closed',
+            id: 2,
         },
         {
             name: 'Jubilee',
             status: 'Good service',
+            id: 1,
         },
         {
             name: 'Metropolitan',
             status: 'Partially closed',
+            id: 2,
         },
         {
             name: 'Northern',
             status: 'Good service',
+            id: 1,
         },
         {
             name: 'Picadilly',
             status: 'Good service',
+            id: 1,
         },
         {
             name: 'Victoria',
             status: 'Good service',
+            id: 1,
         },
         {
             name: 'Waterloo & city',
             status: 'Closed',
+            id: 3,
         },
         {
             name: 'London Overground',
             status: 'Good service',
+            id: 1,
         },
         {
             name: 'Tfl Rail',
             status: 'Good service',
+            id: 1,
         },
         {
             name: 'DLR',
             status: 'Good service',
+            id: 1,
         },
         {
             name: 'Tram',
             status: 'Good service',
+            id: 1,
         },
     ]
 }
@@ -92,12 +107,7 @@ $(function () {
     function addStatusColors(data) {
 
         for (i = 0; i < data.lines.length; i++) {
-            if (data.lines[i].status === 'Partially closed') {
-                $('.tube__status').css('color', '#ff4560');
-            }
-            if (data.lines[i].status === 'Closed') {
-                $('.tube__status').css('color', '#cb2000');
-            }
+
         }
     }
 
