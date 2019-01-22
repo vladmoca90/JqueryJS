2c55bea7268ee903a6ae4c1f628e142d6508a680
Vlad Mocanu <vladmoca90@gmail.com>
2019-01-22 23:35:57 +0000

    Changed class and id names

--- scripts/JQuery/dropdowns/tabs-dropdown.js
+++ scripts/JQuery/dropdowns/tabs-dropdown.js
@@ -1,12 +1,23 @@
 $(function () {
 
-    var cars = $('#carsMakesModels');
-    var vans = $('#vansMakesModels');
+    var cars = $('#carsTab');
+    var vans = $('#vansTab');
 
     vans.hide();
     cars.show();
 
+    $('input').change(function (e) {
+        var inputField = $(this);
+        var id = inputField.attr('id');
+        // hide all tabs
+        $('tab').hide();
+        // show selected tab
+        $(id + 'Tab').show();
+    })
+
     $('.form-tabs label').click(function () {
+        console.log('click was executed');
+
         if ($('#carsTab').is(':checked')) {
             vans.hide();
             cars.show();
