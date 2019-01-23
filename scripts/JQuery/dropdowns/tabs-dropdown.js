6a94ebca54948d3cb0e614fb11c99734cc5f91c5
Vlad Mocanu <vladmoca90@gmail.com>
2019-01-23 20:37:29 +0000

    Rewrote the code without ids

--- scripts/JQuery/dropdowns/tabs-dropdown.js
+++ scripts/JQuery/dropdowns/tabs-dropdown.js
@@ -1,8 +1,14 @@
 $(function () {
 
-    $('#carMakesSelector').change(function() {
-		var value = $(this).find(':checked').attr('value');
-		$('.search-by-model option').hide();
-		$('#carModelsSelector option[data-parent=' + value +']' ).show();
-   });
+    $('#carsTab .search-by-make').change(function () {
+        var value = $(this).find(':selected').attr('value');
+        $('.search-by-model option').hide();
+        $('#carsTab .search-by-model').attr('disabled', false);
+
+        if (value == 'any') {
+            $('#carsTab .search-by-model').attr('disabled', true);
+        }
+
+        $('#carsTab .search-by-model option[data-parent=' + value + ']').show();
+    });
 });
