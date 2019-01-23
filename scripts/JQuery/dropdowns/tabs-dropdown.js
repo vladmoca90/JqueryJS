c7b59638c22892da03b156eb68eeadd7253d66b5
Vlad Mocanu <vladmoca90@gmail.com>
2019-01-23 20:25:51 +0000

    created the filter

--- scripts/JQuery/dropdowns/tabs-dropdown.js
+++ scripts/JQuery/dropdowns/tabs-dropdown.js
@@ -1,22 +1,8 @@
 $(function () {
 
-    var allCars = [];
-    var carMakes = [];
-    var allCars = { "makes": [] };
-
-    function getCarMakes(carMakes) {
-        if (!carMakes) {
-            throw new Error('Car makes must be provided');
-        }
-
-        var getMakes = $('#carsTab').find('.search-by-make option');
-
-        for (var i = 0; i < getMakes.length; i++) {
-            carMakes.push($('.search-by-make').find('option').eq(i).attr('value'));
-        }
-
-        return carMakes;
-    }
-
-    getCarMakes(carMakes);
+    $('#carMakesSelector').change(function() {
+		var value = $(this).find(':checked').attr('value');
+		$('.search-by-model option').hide();
+		$('#carModelsSelector option[data-parent=' + value +']' ).show();
+   });
 });
