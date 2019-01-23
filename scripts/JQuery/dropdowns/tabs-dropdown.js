ba12db84d233d2e472479bb137b8c03f9cb58397
Vlad Mocanu <vladmoca90@gmail.com>
2019-01-23 21:51:02 +0000

    working on tabs

--- scripts/JQuery/dropdowns/tabs-dropdown.js
+++ scripts/JQuery/dropdowns/tabs-dropdown.js
@@ -1,14 +1,30 @@
 $(function () {
 
-    $('#carsTab .search-by-make').change(function () {
+    $('#vansTab').hide();
+
+    $('.form-tabs input').change(function () {
+        var tabId = $(this).attr(id);
+        $('.tab').hide();
+
+        $(tabId + 'Tab').show();
+    });
+
+    $('.search-by-make').change(function () {
         var value = $(this).find(':selected').attr('value');
-        $('.search-by-model option').hide();
-        $('#carsTab .search-by-model').attr('disabled', false);
 
         if (value == 'any') {
-            $('#carsTab .search-by-model').attr('disabled', true);
+            $('.search-by-model [data-parent=any]').prop('selected', true); // select "any" from second dropdown
+            $('.search-by-model option').show();
+            $('.search-by-model').attr('disabled', true);
+
+            return;
         }
 
-        $('#carsTab .search-by-model option[data-parent=' + value + ']').show();
+        $('.search-by-model option').hide();
+        $('.search-by-model').attr('disabled', false);
+
+        $('.search-by-model [data-parent=any]').prop('selected', true);
+        $('.search-by-model [data-parent=any]').show();
+        $('.search-by-model option[data-parent=' + value + ']').show();
     });
 });
