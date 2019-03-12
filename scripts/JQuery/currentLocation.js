877f7a935d89b60672951353529efe22346918ed
Vlad Mocanu <vladmoca90@gmail.com>
2019-03-12 23:46:30 +0000

    Removed the variable

--- scripts/JQuery/currentLocation.js
+++ scripts/JQuery/currentLocation.js
@@ -1,16 +1,15 @@
 $(function () {
-    var x = $('#demo');
 
     function getLocation() {
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(showPosition);
         } else {
-            x.text() == "Geolocation is not supported by this browser.";
+            $('#demo').html("Geolocation is not supported by this browser.");
         }
     }
 
     function showPosition(position) {
-        x.text() == position.coords.latitude + ' ' + position.coords.longitude;
+        $('#demo').html(position.coords.latitude + ' ' + position.coords.longitude);
     }
 
      getLocation();
