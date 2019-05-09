243a044550ebdf2d1e865abe62c9b7dcb8122367
Vlad Mocanu <vladmoca90@gmail.com>
2019-05-09 22:08:35 +0100

    Added a js file

---
+++ scripts/content.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $('#menu').on('click', 'a', function () {
+        $('.current').not($(this).closest('li').addClass('current')).removeClass('current');
+        // fade out all open subcontents
+        $('.pbox:visible').hide(600);
+        // fade in new selected subcontent
+        $('.pbox[id=' + $(this).attr('data-id') + ']').show(600);
+    });
+});
