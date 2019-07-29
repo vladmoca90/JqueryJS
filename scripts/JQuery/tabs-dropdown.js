7c55f8085fd652b37f096d95df6faa27005bc95a
Vlad Mocanu <vladmoca90@gmail.com>
2019-07-29 23:43:29 +0100

    Created two arrays of objects with cars and vans

--- scripts/JQuery/tabs-dropdown.js
+++ scripts/JQuery/tabs-dropdown.js
@@ -1,3 +1,58 @@
+var cars = [
+    {
+      make: 'Abarth',
+      model: ['Abarth 1', 'Abarth 2', 'Abarth 3'],
+    },
+    {
+      make: 'Alfa Romeo',
+      model: ['Alfa Romeo 1', 'Alfa Romeo 2', 'Alfa Romeo 3'],
+    },
+    {
+      make: 'Toyota',
+      model: ['Toyota 1', 'Toyota 2', 'Toyota 3'],
+    },
+    {
+      make: 'Vauxhall',
+      model: ['Vauxhall 1', 'Vauxhall 2', 'Vauxhall 3'],
+    },
+    {
+      make: 'Volkswagen',
+      model: ['Volkswagen 1', 'Volkswagen 2', 'Volkswagen 3'],
+    },
+    {
+      make: 'Volvo',
+      model: ['Volvo 1', 'Volvo 2', 'Volvo 3'],
+    },
+];
+
+var vans = [
+    {
+      make: 'Abarth',
+      model: ['Abarth 11', 'Abarth 21', 'Abarth 31'],
+    },
+    {
+      make: 'Alfa Romeo',
+      model: ['Alfa Romeo 11', 'Alfa Romeo 21', 'Alfa Romeo 31'],
+    },
+    {
+      make: 'Toyota',
+      model: ['Toyota 11', 'Toyota 21', 'Toyota 31'],
+    },
+    {
+      make: 'Vauxhall',
+      model: ['Vauxhall 11', 'Vauxhall 21', 'Vauxhall 31'],
+    },
+    {
+      make: 'Volkswagen',
+      model: ['Volkswagen 11', 'Volkswagen 21', 'Volkswagen 31'],
+    },
+    {
+      make: 'Volvo',
+      model: ['Volvo 11', 'Volvo 21', 'Volvo 31'],
+    },
+];
+
+
 $(function () {
 
     $('#vansTab').hide();
