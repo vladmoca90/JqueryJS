13d014e9ec637444d897faaada584f52d71a7469
Vlad Mocanu <vladmoca90@gmail.com>
2019-07-30 22:48:56 +0100

    The API works

--- scripts/JQuery/AJAX/cities.js
+++ scripts/JQuery/AJAX/cities.js
@@ -1,10 +1,18 @@
 $(function() {
+
+    function getCity(results) {
+
+    }
+
     citiesUrl = 'https://api.meetup.com/2/cities';
 
     $.ajax(citiesUrl, {
         method:'GET',
         success: function(response) {
             console.log(response);
+            results = response.results;
+
+            getCity(response);
         },
         error: function(err) {
             console.error(err);
