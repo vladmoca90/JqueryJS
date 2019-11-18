f8596693ecc4565f44619ef05847f2029e5159c2
Vlad Mocanu <vladmoca90@gmail.com>
2019-11-18 23:33:37 +0000

    Added variables

--- scripts/JQuery/textarea-word-count.js
+++ scripts/JQuery/textarea-word-count.js
@@ -1,13 +1,14 @@
 $(function () {
     var words = $('#maxWords');
+    var textBlock = $('#textarea');
 
-    $('#textarea').on('keyup', function () {
+    textBlock.on('keyup', function () {
         var count = $(this).val().length;
 
         if(count > 1000) {
             throw new Error('you have more words than the limmit');
         }
 
-        words.text(count + '/' + $('#textarea').attr('maxlength'));
+        words.text(count + '/' + textBlock.attr('maxlength'));
     });
 });
