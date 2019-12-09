0b929cc53a925e2b400d78ef20fb0cb2963d7d65
Vlad Mocanu <vladmoca90@gmail.com>
2019-12-09 23:43:34 +0000

    Test

--- scripts/JQuery/monthly-prices.js
+++ scripts/JQuery/monthly-prices.js
@@ -4,6 +4,8 @@ $(function () {
     for (let i = 0; i < vehicles.length; i++) {
         let vehicle = $(vehicles[i]);
 
+        let vehPrice = vehicle.children('.car-price').html().trim().substring(1); //gives '5,999' for example
     }
 
+    return parseInt(vehPrice);
 });
