efd9315d7264b79a20dc5e60a734d26eb950bd3a
Vlad Mocanu <vladmoca90@gmail.com>
2020-01-26 04:06:42 +0000

    Deleted spaces

--- scripts/JQuery/monthly-prices.js
+++ scripts/JQuery/monthly-prices.js
@@ -8,11 +8,8 @@ $(function () {
 
         for (let i = 0; i < vehicles.length; i++) {
             let vehicle = $(vehicles[i]);
-
             let vehPrice = vehicle.children('.car-price').html().replace(',', '').trim().substring(1);
-
             let price = parseInt(vehPrice);
-
             let monthlyPrice = (price/12).toFixed(2);
 
             vehicle.children('.monthly-price').text('£' + monthlyPrice + '/month');
