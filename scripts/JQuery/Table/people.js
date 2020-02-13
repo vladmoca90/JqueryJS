8b14fa7613853a95ebd270810c09221dc1cc9975
Vlad Mocanu <vladmoca90@gmail.com>
2020-02-13 00:10:18 +0000

    Created a .scss file and deleted the styling jquery

--- scripts/JQuery/Table/people.js
+++ scripts/JQuery/Table/people.js
@@ -1,24 +1,13 @@
 ﻿$(function () {
 
-    $("table").width(450).addClass("table table-hover").css({
-        "margin": "20px auto",
-        "border": "1px solid #ccc"
-    });
+    var data = [
+        {
+            firstName: '',
+            lastName: '',
+            age: '',
+            nationality: '',
+        }
+    ];
 
-    $("tr>td").css("border-right", "1px solid #DDDDDD").not(".country");
-
-    $("td").css("cursor", "pointer");
-
-    $("th, td").addClass("text-center").css({
-        "font-size": "16px",
-        "font-family": "Calibri",
-    });
-
-    $(".country").css({
-        "font-weight": "bold",
-        "font-style": "italic"
-    });
-
-    $('tr[title="Thomas Nicholas"], tr[title="Alexandru Anton"], tr[title="Julie Sparks"], tr[title="Katrin Basch"]').addClass("warning");
-
+  
 });
