8db6acf949f259e0041db28d9c2d32b952abd4d1
Vlad Mocanu <vladmoca90@gmail.com>
2020-03-16 21:57:09 +0000

    Made the click effect

--- scripts/JQuery/weekdays.js
+++ scripts/JQuery/weekdays.js
@@ -1,8 +1,9 @@
 $(function() {
     var day = $('#selectable').find('li');
+    var showDay = $('#showDay');
 
     day.on('click', function() {
         $(this).addClass('clicked');
-        $('#showDay').text($(this).text());
+        showDay.text($(this).text());
     });
 });
