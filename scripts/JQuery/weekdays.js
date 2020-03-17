d42871b2238c851d13b1b8c87837c2c48416822f
Vlad Mocanu <vladmoca90@gmail.com>
2020-03-17 22:57:16 +0000

    Working

--- scripts/JQuery/weekdays.js
+++ scripts/JQuery/weekdays.js
@@ -9,20 +9,6 @@ $(function () {
     });
 
     exclude.on('change', function () {
-        exclude.attr('checked', 'checked');
-
-        if (exclude.prop('checked')) {
-            for (var i = 0; i < day.length; i++) {
-                var content = day.eq(i).text();
-
-                if (content === 'Saturday' || content === 'Sunday') {
-                    day.addClass('disable-day');
-                }
-            }
-        }
+        day.text() === 'Saturday' || day.text() === 'Sunday' ? day.addClass('disable-day') : day.removeClass('disable-day');
     });
 });
-
-//  if (day.text() === 'Saturday' || day.text() === 'Sunday') {
-//     day.addClass('disable-day');
-// }
