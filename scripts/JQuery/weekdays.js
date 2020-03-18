bfca8722b31b20722a1f2406f7063b00808d3d0b
Vlad Mocanu <vladmoca90@gmail.com>
2020-03-18 00:35:32 +0000

    Tested

--- scripts/JQuery/weekdays.js
+++ scripts/JQuery/weekdays.js
@@ -10,17 +10,8 @@ $(function () {
 
     exclude.on('change', function () {
         if (this.checked) {
-            day.addClass('disable-day');
-            showDay.hide();
-        } else {
-            day.removeClass('clicked');
-            day.removeClass('disable-day');
-
-            // if(day.hasClass('disable-day') == true) {
-            //     return showDay.html(' ');
-            // }
-
-            showDay.show();
+            day.eq(5, 6).addClass('disable-day');
+            // showDay.hide();
         }
     });
 });
