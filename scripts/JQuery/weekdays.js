a4e4b7d927e3aa72042f1711aabce30f6de690c3
Vlad Mocanu <vladmoca90@gmail.com>
2020-03-18 03:30:38 +0000

    It works. It selects the weekend only

--- scripts/JQuery/weekdays.js
+++ scripts/JQuery/weekdays.js
@@ -14,7 +14,11 @@ $(function () {
 
     exclude.on('change', function () {
         if (this.checked) {
-          
+            for(var i = 0; i < day.length; i++) {
+                if(day.eq(i).text() === 'Saturday' || day.eq(i).text() === 'Sunday') {
+                    day.eq(i).addClass('disable-day');
+                }
+            }
         }
     });
 });
