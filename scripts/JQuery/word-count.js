028d46e95babde70478b83a6a07411d81d13e7f8
Vlad Mocanu <vladmoca90@gmail.com>
2020-03-20 02:35:26 +0000

    Will add a condition to avoid the counting of empty spaces

--- scripts/JQuery/word-count.js
+++ scripts/JQuery/word-count.js
@@ -1,14 +1,14 @@
 $(function () {
     var words = $('#maxWords');
-    var textBlock = $('#textarea');
+    var field = $('#textarea');
 
-    textBlock.on('keyup', function () {
+    field.on('keyup', function () {
         var count = $(this).val().length;
 
         if(count > 100) {
             throw new Error('you have more words than the 1000 words limit');
         }
 
-        words.text(count + '/' + textBlock.attr('maxlength'));
+        words.text(count + ' / ' + field.attr('maxlength'));
     });
 });
