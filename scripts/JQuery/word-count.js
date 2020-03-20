d24c54b65964dd0f8fa847f9abffcf72dab6d684
Vlad Mocanu <vladmoca90@gmail.com>
2020-03-20 02:37:16 +0000

    Has a bug in the counting

--- scripts/JQuery/word-count.js
+++ scripts/JQuery/word-count.js
@@ -3,12 +3,12 @@ $(function () {
     var field = $('#textarea');
 
     field.on('keyup', function () {
-        var count = $(this).val().length;
+        var count = $(this).val().trim().replace(' ', '');
 
         if(count > 100) {
             throw new Error('you have more words than the 1000 words limit');
         }
 
-        words.text(count + ' / ' + field.attr('maxlength'));
+        words.text(count.length + ' / ' + field.attr('maxlength'));
     });
 });
