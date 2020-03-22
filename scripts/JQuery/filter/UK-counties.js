be587c670961f22be427cf9ce0cbfd9969209d40
Vlad Mocanu <vladmoca90@gmail.com>
2020-03-22 04:43:54 +0000

    Adjusted the styling

--- scripts/JQuery/filter/UK-counties.js
+++ scripts/JQuery/filter/UK-counties.js
@@ -1,6 +1,5 @@
 var data = {
-    counties: [
-        {
+    counties: [{
             title: 'Berskshire',
             content: 'Berkshire is a county in south east England, west of London and is one of the home counties. It was recognised by the Queen as the Royal County of Berkshire in 1957 because of the presence of Windsor Castle, and letters patent were issued in 1974. Berkshire is a county of historic origin and is a home county',
         },
@@ -15,28 +14,35 @@ var data = {
     ],
 }
 
-const addCounties = data => {
-    if (data.counties.length == 0) {
-        throw new Error('The data is empty');
+$(function () {
+
+    const addCounties = data => {
+        if (data.counties.length == 0) {
+            throw new Error('The data is empty');
+        }
+
+        for (var i = 0; i < data.counties.length; i++) {
+            let wrapper = $('<div class="wrapper"></div>');
+            let container = $('.counties__container');
+            let heading = $('<h3 class="counties__heading"></h3>');
+            let text = $('<p class="counties__text"></p>');
+
+            heading.text(data.counties[i].title);
+            text.text(data.counties[i].content);
+
+            container.append(wrapper);
+            wrapper.append(heading);
+            wrapper.append(text);
+        }
     }
 
-    for (var i = 0; i < data.counties.length; i++) {
-        let wrapper = $('<div class="wrapper"></div>');
-        let container = $('.counties__container');
-        let heading = $('<h3 class="counties__heading"></h3>');
-        let text = $('<p class="counties__text"></p>');
+    addCounties(data);
 
-        heading.text(data.counties[i].title);
-        text.text(data.counties[i].content);
+    $(".search-bar").on("keyup", function () {
+        var value = $(this).val().toLowerCase();
+        $("#myList li").filter(function () {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+    });
 
-        container.append(wrapper);
-        wrapper.append(heading);
-        wrapper.append(text);
-    }
-}
-
-addCounties(data);
-
-// const filterText = (data) => {
-//     return data.filter((data) => return );
-// }
+});
