5a2e4a77d2ce20a27bc17ee888acc85c32df8864
Vlad Mocanu <vladmoca90@gmail.com>
2020-03-24 23:29:12 +0000

    Numbered the products

--- scripts/JQuery/Filter/products.js
+++ scripts/JQuery/Filter/products.js
@@ -1,15 +1,16 @@
 $(function () {
-    var products = [];
+    let products = [];
+    let list = $('#productsList');
 
     function createProductsList(products) {
         if (!products) {
             throw new Error('The products parameter must be populated');
         }
 
-        var productsList = $('.products-container').find('.product');
+        let productsList = $('.products-container').find('.product');
 
         for (var i = 0; i < productsList.length; i++) {
-            var productsObj = new Object();
+            let productsObj = new Object();
 
             productsObj.name = $('.product').eq(i).attr('data-product-name'),
             productsObj.id = $('.product').eq(i).attr('data-product-id'),
@@ -25,12 +26,12 @@ $(function () {
             throw new Error('The products list must be given');
         }
 
-        $('#productsList option:nth-of-type(1)').val(0);
+        list.find('option').eq(0).val(0);
 
         for (var i = 0; i < products.length; i++) {
-            var option = $('<option></option>');
+            let option = $('<option></option>');
 
-            $('#productsList').append(option);
+            list.append(option);
             option.attr('value', products[i].id);
             option.val(products[i].id);
             option.html(products[i].name);
@@ -39,13 +40,13 @@ $(function () {
 
     addProductsToList(products);
 
-    $('#productsList').change(function () {
-        var selected = $(this).find(':selected');
-        var selectedOption = selected.val();
+    list.change(function () {
+        let selected = $(this).find(':selected');
+        let selectedOption = selected.val();
 
         $('.product').hide();
 
-        var matchedValue = `.product[data-product-id=${selectedOption}]`;
+        let matchedValue = '.product[data-product-id=' + selectedOption + ']';
 
         if(selectedOption == 0) {
             $('.product').show();
