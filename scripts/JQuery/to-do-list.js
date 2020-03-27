b0ab87f286a02a3df631f44e08e2d0c78cf9da51
Vlad Mocanu <vladmoca90@gmail.com>
2020-03-27 23:22:56 +0000

    Started working on the code

--- scripts/JQuery/to-do-list.js
+++ scripts/JQuery/to-do-list.js
@@ -1,4 +1,12 @@
 $(function(){
     let text = $('#textField');
     let content = $('#textField').val();
+    let button = $('button');
+
+    button.on('click', function() {
+        var listItem= $('<li></li>');
+
+        listItem.text(content);
+        $('.list-container').find('ul').append(listItem.text(content));
+    });
 });
