4f414c71e30cf9631313cd33e9d6bd927f6cfaff
Vlad Mocanu <vladmoca90@gmail.com>
2020-03-28 23:46:50 +0000

    Declared the close element at the top of the code

--- scripts/JQuery/to-do-list.js
+++ scripts/JQuery/to-do-list.js
@@ -1,6 +1,7 @@
 $(function () {
     const button = $('button');
     let list = $('.list-container');
+    let close = $('<i class="fa fa-close"></i>');
 
     button.on('click', function () {
         let text = $('#textField');
@@ -9,17 +10,17 @@ $(function () {
         let listText = listItem.text(listContent);
 
         list.find('ul').append(listText);
-        listItem.append('<i class="fa fa-close"></i>');
+        listItem.append(close);
 
         if (listContent.length == 0 || listContent === '') {
             listItem.remove();
         }
-        if(listContent.length > 0) {
+        if (listContent.length > 0) {
             return text.val(null);
         }
+    });
 
-        $('.fa-close').on('click', function() {
-            $(this).parent().remove();
-        });
+    $('.fa-close').on('click', function () {
+        $(this).parent().remove();
     });
 });
