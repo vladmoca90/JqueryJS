8c5e0e316c116046529cda6f9418306bb602a81c
Vlad Mocanu <vladmoca90@gmail.com>
2020-04-17 23:53:34 +0100

    Added the correct font size

--- scripts/JQuery/vowels-consonants.js
+++ scripts/JQuery/vowels-consonants.js
@@ -2,6 +2,8 @@ $(function() {
     let text = $('#textField');
 
     function returnLetters(text) {
-
+        if(text.length == 0) {
+            throw new Error('The field cannot be empty');
+        }
     }
 });
