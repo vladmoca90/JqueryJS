35d0a4ad6b8203d485e0bfcb4eec4ce0f5fc599c
Vlad Mocanu <vladmoca90@gmail.com>
2020-04-18 22:57:01 +0100

    Added a click event

--- scripts/JQuery/vowels-consonants.js
+++ scripts/JQuery/vowels-consonants.js
@@ -1,27 +1,30 @@
 $(function () {
     let text = $('#textField');
+    let total = text.val();
+    let check = $('.check-btn');
 
-    const returnLetters = text => {
-        if (text.length == 0) {
-            throw new Error('The field cannot be empty');
-        }
+    check.on('click', function () {
+        const returnLetters = total => {
+            if (total.length == 0) {
+                throw new Error('The field cannot be empty');
+            }
 
-        let count = 0;
+            let count = 0;
 
-        for (let i = 0; i < text.length; i++) {
-            if (isVowel(c)) {
-                count++;
-            } else {
-
+            for (let i = 0; i < total.length; i++) {
+                if (isVowel(c)) {
+                    count++;
+                    $('.vowels-number').text(count);
+                }
             }
         }
-    }
 
-    const isVowel = c => {
-        let v = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
+        const isVowel = c => {
+            let v = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
 
-        return v;
-    }
+            return v;
+        }
 
-    returnLetters(text);
+        returnLetters(total);
+    });
 });
