4f487e5ee43049d059fe584d362e9fed49c46e6e
Vlad Mocanu <vladmoca90@gmail.com>
2020-04-18 20:02:06 +0100

    Fixed a function call bug

--- scripts/JQuery/vowels-consonants.js
+++ scripts/JQuery/vowels-consonants.js
@@ -17,11 +17,11 @@ $(function () {
         }
     }
 
-    returnLetters(text);
-
     const isVowel = c => {
         let v = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
 
         return v;
     }
+
+    returnLetters(text);
 });
