ff955403043165cf1edf5bed25be9a3b949598b6
Vlad Mocanu <vladmoca90@gmail.com>
2020-04-18 19:55:20 +0100

    Started the function

--- scripts/JQuery/vowels-consonants.js
+++ scripts/JQuery/vowels-consonants.js
@@ -1,9 +1,21 @@
-$(function() {
+$(function () {
     let text = $('#textField');
 
     function returnLetters(text) {
-        if(text.length == 0) {
+        if (text.length == 0) {
             throw new Error('The field cannot be empty');
         }
+
+        let count = 0;
+
+        for (let i = 0; i < text.length; i++) {
+            if() {
+                count++;
+            } else {
+                
+            }
+        }
     }
+
+    returnLetters(text);
 });
