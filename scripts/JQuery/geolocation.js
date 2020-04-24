e0aa043538a98191d476b8bd40b57da334ef79b8
Vlad Mocanu <vladmoca90@gmail.com>
2020-04-24 23:23:59 +0100

    Replaces the coordinates with 2 let variabls

--- scripts/JQuery/geolocation.js
+++ scripts/JQuery/geolocation.js
@@ -2,11 +2,15 @@ $(function () {
     let coordinates = $('#coordinates');
 
     const getCurrentLocation = () => {
-        navigator.geolocation ? navigator.geolocation.getCurrentPosition(showPosition) : coordinates.html("Geolocation is not supported by this browser.");
+        navigator.geolocation ? navigator.geolocation.getCurrentPosition(showPosition) 
+                              : coordinates.html('Geolocation is not supported by this browser.');
     }
 
     const showPosition = position => {
-        coordinates.html(position.coords.latitude + ' ' + position.coords.longitude);
+        let currentLat = position.coords.latitude;
+        let currentLong = position.coords.latitude;
+
+        coordinates.html(currentLat.toFixed(4) + ' ' + currentLong.toFixed(4));
     }
 
     getCurrentLocation();
