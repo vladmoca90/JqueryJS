7c15658c618aa8f7ad513fcac3911abaea47ac4c
Vlad Mocanu <vladmoca90@gmail.com>
2020-04-24 23:15:51 +0100

    Rewrote with ternary operation

--- scripts/JQuery/geolocation.js
+++ scripts/JQuery/geolocation.js
@@ -1,17 +1,13 @@
 $(function () {
     let coordinates = $('#coordinates');
 
-    const getLocation = () => {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(showPosition);
-        } else {
-            coordinates.html("Geolocation is not supported by this browser.");
-        }
+    const getCurrentLocation = () => {
+        navigator.geolocation ? navigator.geolocation.getCurrentPosition(showPosition) : coordinates.html("Geolocation is not supported by this browser.");
     }
 
-    const showPosition = (position) => {
+    const showPosition = position => {
         coordinates.html(position.coords.latitude + ' ' + position.coords.longitude);
     }
 
-     getLocation();
+    getCurrentLocation();
 });
