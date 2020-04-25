68e3cc93221ad97c38eb19e64358a6c7fcea3f38
Vlad Mocanu <vladmoca90@gmail.com>
2020-04-25 23:27:09 +0100

    Changed var to let

--- scripts/JQuery/likes-dislikes.js
+++ scripts/JQuery/likes-dislikes.js
@@ -1,41 +1,41 @@
-$(function() {
-    $('.like-button').click(function() {
-        var parent = $(this).parent();
-        var count = parseInt(parent.attr("data-count"));
-        var liked = parent.attr("data-liked") == 'true';
+$(function () {
+    $('.like-button').click(function () {
+        let parent = $(this).parent();
+        let count = parseInt(parent.attr('data-count'));
+        let liked = parent.attr('data-liked') == 'true';
 
-        if(liked) { // if we've already liked this, then it means we will unlike it now
-            $(this).html("Like");
+        if (liked) { // if we've already liked this, then it means we will unlike it now
+            $(this).html('Like');
             liked = false;
             count = count - 1;
             parent.attr('data-liked', liked);
             parent.attr('data-count', count);
 
-            var text = "";
+            let text = '';
 
-            if(count == 0) {
-                text = "Be the first to like this!";
+            if (count == 0) {
+                text = 'Be the first to like this!';
             } else {
-                text = count == 1 ? "1 other person likes this" : count + " others like this";
+                text = count == 1 ? '1 other person likes this' : count + ' others like this';
             }
 
-            parent.find("span").html(text);
+            parent.find('span').html(text);
         } else { // the other way round
-            $(this).html("Unlike");
+            $(this).html('Unlike');
             liked = true;
             count = count + 1;
             parent.attr('data-liked', liked);
             parent.attr('data-count', count);
 
-            var text = "";
+            let text = '';
 
-            if(count == 1) {
-                text = "You like this";
+            if (count == 1) {
+                text = 'You like this';
             } else {
-                text = count == 2 ? "You and one other person likes this" : "You and " + (count-1) + " others like this";
+                text = count == 2 ? 'You and one other person likes this' : 'You and ' + (count - 1) + ' others like this';
             }
 
-            parent.find("span").html(text);
+            parent.find('span').html(text);
         }
     })
-})
+});
