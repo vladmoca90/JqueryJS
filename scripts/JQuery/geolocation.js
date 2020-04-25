922f8d9490bd91edaf714a6c0d0dbd7825dfdcf6
Vlad Mocanu <vladmoca90@gmail.com>
2020-04-25 23:01:18 +0100

    Deleted the button

--- scripts/JQuery/geolocation.js
+++ scripts/JQuery/geolocation.js
@@ -2,9 +2,11 @@ $(function () {
     let coordinates = $('#coordinates');
 
     const getCurrentLocation = () => {
-        navigator.geolocation ? navigator.geolocation.getCurrentPosition(showPosition) 
-                              : coordinates.html('Geolocation is not supported by this browser.');
+        navigator.geolocation ? navigator.geolocation.getCurrentPosition(showPosition) :
+            coordinates.html('Geolocation is not supported by this browser.');
     }
+
+    getCurrentLocation();
 
     const showPosition = position => {
         let currentLat = position.coords.latitude;
@@ -13,5 +15,5 @@ $(function () {
         coordinates.html(currentLat.toFixed(4) + ' ' + currentLong.toFixed(4));
     }
 
-    getCurrentLocation();
+    showPosition(position);
 });
