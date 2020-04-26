56c6e74c2135a5af5604845bb6a7bae4d9388db2
Vlad Mocanu <vladmoca90@gmail.com>
2020-04-26 21:51:59 +0100

    Rewrote the function with arrow function and used let instead of var

--- scripts/JQuery/dropdowns/people.js
+++ scripts/JQuery/dropdowns/people.js
@@ -1,4 +1,4 @@
-var data = {
+const data = {
     persons: [{
             name: 'Ana-Maria Magureanu',
             age: '29',
@@ -28,20 +28,20 @@ var data = {
 }
 
 $(function () {
-    var button = $('.dropdown__button');
-    var list = $('.dropdown__list');
+    let button = $('.dropdown__button');
+    let list = $('.dropdown__list');
 
     button.on('click', function () {
         list.slideToggle(250);
     });
 
-    function addNames(data) {
+    const addNames = data => {
         if (data.persons.length == 0) {
             throw new Error('The persons list must be given');
         }
 
-        for (i = 0; i < data.persons.length; i++) {
-            var name = $('<li class="dropdown__list-item"></li>');
+        for (let i = 0; i < data.persons.length; i++) {
+            let name = $('<li class="dropdown__list-item"></li>');
 
             name.val(data.persons[i].name);
             name.html(data.persons[i].name);
