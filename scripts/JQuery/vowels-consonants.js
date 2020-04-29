ee36c12945f54345c02cb3fc58cf9ce3aad01fb9
Vlad Mocanu <vladmoca90@gmail.com>
2020-04-29 02:20:03 +0100

    The conditions are valid now and rewrote the code

--- scripts/JQuery/vowels-consonants.js
+++ scripts/JQuery/vowels-consonants.js
@@ -4,33 +4,12 @@ $(function () {
     let text = field.val();
     $('.alert-text').hide();
 
-    const returnLetters = text => {
+    check.on('click', function () {
         if (text.length == 0) {
             $('.alert-text').show();
         }
         if (text.length > 0) {
             $('.alert-text').hide();
         }
-    }
-
-    returnLetters(text);
-
-    check.on('click', function () {
-
-
-        // let vowels = [];
-
-        // for (let i = 0; i < text; i++) {
-        // if(isVowel(c)) {
-        //     vowels.push(c);
-        // } else {
-        //     cons.push(c);
-        // }
-        // }
-        // const isVowel = c => {
-        //     let v = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
-
-        //     return v;
-        // }
     });
 });
