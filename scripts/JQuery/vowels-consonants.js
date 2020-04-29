de3510e128078670369edbbbb7e885e2f7ea85d6
Vlad Mocanu <vladmoca90@gmail.com>
2020-04-29 01:36:08 +0100

    Fixed the conditions

--- scripts/JQuery/vowels-consonants.js
+++ scripts/JQuery/vowels-consonants.js
@@ -4,31 +4,33 @@ $(function () {
     let text = field.val();
     $('.alert-text').hide();
 
+    const returnLetters = text => {
+        if (text.length == 0) {
+            $('.alert-text').show();
+        }
+        if (text.length > 0) {
+            $('.alert-text').hide();
+        }
+    }
+
+    returnLetters(text);
+
     check.on('click', function () {
-        const returnLetters = text => {
-            if (text.length == 0) {
-                $('.alert-text').hide();
-            } 
-            if (text.length > 0) {
-                $('.alert-text').show();
-            } 
-        }
 
-        let vowels = [];
 
-        for(let i = 0; i < text; i++) {
-            // if(isVowel(c)) {
-            //     vowels.push(c);
-            // } else {
-            //     cons.push(c);
-            // }
-        }
-        const isVowel = c => {
-            let v = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
+        // let vowels = [];
 
-            return v;
-        }
+        // for (let i = 0; i < text; i++) {
+        // if(isVowel(c)) {
+        //     vowels.push(c);
+        // } else {
+        //     cons.push(c);
+        // }
+        // }
+        // const isVowel = c => {
+        //     let v = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
 
-        returnLetters(text);
+        //     return v;
+        // }
     });
 });
