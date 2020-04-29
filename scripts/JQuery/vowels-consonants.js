ff49311b0fd3daae29eb72f1e6e709b54aad6499
Vlad Mocanu <vladmoca90@gmail.com>
2020-04-29 01:23:52 +0100

    Working on it

--- scripts/JQuery/vowels-consonants.js
+++ scripts/JQuery/vowels-consonants.js
@@ -3,6 +3,7 @@ $(function () {
     let field = $('#textField');
     let text = field.val();
     $('.alert-text').hide();
+
     check.on('click', function () {
         const returnLetters = text => {
             if (text.length == 0) {
@@ -11,6 +12,16 @@ $(function () {
             if (text.length > 0) {
                 $('.alert-text').show();
             } 
+        }
+
+        let vowels = [];
+
+        for(let i = 0; i < text; i++) {
+            // if(isVowel(c)) {
+            //     vowels.push(c);
+            // } else {
+            //     cons.push(c);
+            // }
         }
         const isVowel = c => {
             let v = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
