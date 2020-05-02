beab95fdf3fd0cdbdaef257b5c02aae94c1d94bb
Vlad Mocanu <vladmoca90@gmail.com>
2020-05-02 02:19:19 +0100

    Finished a code sample. Will test it.

--- scripts/JQuery/vowels-consonants.js
+++ scripts/JQuery/vowels-consonants.js
@@ -5,13 +5,25 @@ $(function () {
     $('.alert-text').hide();
 
     check.on('click', function () {
-        const countVowels = text => {
+        const countEachLetter = text => {
             if (text.length == 0) {
                 $('.alert-text').show();
             }
             if (text.length > 0) {
                 $('.alert-text').hide();
             }
+
+            let count = 0;
+
+            for (let i = 0; i < text.length; i++) {
+                let c = text.charAt(i);
+                if (isVowel(c)) {
+                    count++;
+                }
+            }
+
+            $('.vowels-number').text(count);
+            $('.cons-number').text(text.length - count);
         }
 
         const isVowel = c => {
@@ -20,6 +32,6 @@ $(function () {
             return v;
         }
 
-        countVowels(text);
+        countEachLetter(text);
     });
 });
