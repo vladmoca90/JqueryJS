6dcc8063e7233e3cf7585d8ee513ecd08dd92fc7
Vlad Mocanu <vladmoca90@gmail.com>
2020-05-09 04:39:16 +0100

    Renamed the object and removed some properties

--- scripts/JQuery/people-toggle.js
+++ scripts/JQuery/people-toggle.js
@@ -1,28 +1,23 @@
-const data = {
+const people = {
     persons: [{
             name: 'Ana-Maria Magureanu',
             age: '29',
-            occupation: 'Farmacist',
         },
         {
             name: 'Alexander Swift',
             age: '25',
-            occupation: 'Junior Front End Developer',
         },
         {
             name: 'Caitlin Morgan',
             age: '30',
-            occupation: 'IT Recruiter',
         }, 
         {
             name: 'Vladimir Krushchev',
             age: '35',
-            occupation: 'Design Manager',
         },
         {
             name: 'Leon Williams',
             age: '29',
-            occupation: 'Flex Team Leader',
         },
     ]
 }
@@ -35,19 +30,19 @@ $(function () {
         list.slideToggle(250);
     });
 
-    const addNames = data => {
-        if (data.persons.length == 0) {
+    const addNames = people => {
+        if (people.persons.length == 0) {
             throw new Error('The persons list must be given');
         }
 
-        for (let i = 0; i < data.persons.length; i++) {
+        for (let i = 0; i < people.persons.length; i++) {
             let name = $('<li class="dropdown__list-item"></li>');
 
-            name.val(data.persons[i].name);
-            name.html(data.persons[i].name);
+            name.val(people.persons[i].name);
+            name.html(people.persons[i].name);
             list.append(name);
         }
     }
 
-    addNames(data);
+    addNames(people);
 });
