ff2d466308874fb2b39eab2e020fcc8604a9c6ad
Vlad Mocanu <vladmoca90@gmail.com>
2020-06-08 00:22:02 +0100

    Rewrote as arrow function

--- scripts/JQuery/Table/people.js
+++ scripts/JQuery/Table/people.js
@@ -1,58 +1,59 @@
-﻿$(function () {
+﻿
+const people = [
+    {
+        firstName: 'Vlad',
+        lastName: 'Mocanu',
+        age: '29',
+        nationality: 'Romanian',
+    },
+    {
+        firstName: 'Val',
+        lastName: 'Fellini',
+        age: '50',
+        nationality: 'Italian',
+    },
+    {
+        firstName: 'Leon',
+        lastName: 'Williams',
+        age: '33',
+        nationality: 'British',
+    },
+    {
+        firstName: 'Klaudia',
+        lastName: 'Rudna',
+        age: '36',
+        nationality: 'Polish',
+    },
+];
+$(function () {
 
-    var people = [{
-            firstName: 'Vlad',
-            lastName: 'Mocanu',
-            age: '29',
-            nationality: 'Romanian',
-        },
-        {
-            firstName: 'Val',
-            lastName: 'Fellini',
-            age: '50',
-            nationality: 'Italian',
-        },
-        {
-            firstName: 'Leon',
-            lastName: 'Williams',
-            age: '33',
-            nationality: 'British',
-        },
-        {
-            firstName: 'Klaudia',
-            lastName: 'Rudna',
-            age: '36',
-            nationality: 'Polish',
-        },
-    ];
-
-    function addPeople(people) {
-        if (people.length == 0) {
+    const addPeople = people => {
+        if(people.length == 0) {
             throw new Error('The data must be given');
         }
 
-        for (var i = 0; i < people.length; i++) {
-            var row = $('<tr></tr>');
+        for(let i = 0; i < people.length; i++) {
+            let row = $('<tr></tr>');
 
-            var firstName = $('<td class="first-name"></td>');
+            let firstName = $('<td class="first-name"></td>');
             firstName.val(people[i].firstName);
             firstName.html(people[i].firstName);
 
             row.append(firstName);
 
-            var lastName = $('<td class="last-name"></td>');
+            let lastName = $('<td class="last-name"></td>');
             lastName.val(people[i].lastName);
             lastName.html(people[i].lastName);
 
             row.append(lastName);
 
-            var age = $('<td class="age"></td>');
+            let age = $('<td class="age"></td>');
             age.val(people[i].age);
             age.html(people[i].age);
 
             row.append(age);
 
-            var country = $('<td class="country"></td>');
+            let country = $('<td class="country"></td>');
             country.val(people[i].nationality);
             country.html(people[i].nationality);
 
