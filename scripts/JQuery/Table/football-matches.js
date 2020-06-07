acebcbeed14f71521cbe5bedd526fd7e11169cfc
Vlad Mocanu <vladmoca90@gmail.com>
2020-06-08 00:20:16 +0100

    Rewrote as arrow function

--- scripts/JQuery/Table/football-matches.js
+++ scripts/JQuery/Table/football-matches.js
@@ -1,4 +1,4 @@
-var data = {
+const data = {
     teams: [{
             name: 'Brazil',
             played: 3,
@@ -48,64 +48,64 @@ var data = {
 
 $(function () {
 
-    function getTeams(data) {
-        if (data.teams.length == 0) {
+    const getTeams = data => {
+        if(data.teams.length == 0) {
             throw new Error('The teams must be given');
         }
 
-        for (var i = 0; i < data.teams.length; i++) {
-            var row = $('<tr></tr>');
+        for(let i = 0; i < data.teams.length; i++) {
+            let row = $('<tr></tr>');
 
-            var teamName = $('<td></td>');
+            let teamName = $('<td></td>');
             teamName.addClass('team-name');
             teamName.val(data.teams[i].name);
             teamName.html(data.teams[i].name);
 
             row.append(teamName);
 
-            var playedGames = $('<td></td>');
+            let playedGames = $('<td></td>');
             playedGames.val(data.teams[i].played);
             playedGames.html(data.teams[i].played);
 
             row.append(playedGames);
 
-            var wonGames = $('<td></td>');
+            let wonGames = $('<td></td>');
             wonGames.val(data.teams[i].won);
             wonGames.html(data.teams[i].won);
 
             row.append(wonGames);
 
-            var drawGames = $('<td></td>');
+            let drawGames = $('<td></td>');
             drawGames.val(data.teams[i].draw);
             drawGames.html(data.teams[i].draw);
 
             row.append(drawGames);
 
-            var lostGames = $('<td></td>');
+            let lostGames = $('<td></td>');
             lostGames.val(data.teams[i].lost);
             lostGames.html(data.teams[i].lost);
 
             row.append(lostGames);
 
-            var goalsF = $('<td></td>');
+            let goalsF = $('<td></td>');
             goalsF.val(data.teams[i].goalsFor);
             goalsF.html(data.teams[i].goalsFor);
 
             row.append(goalsF);
 
-            var goalsA = $('<td></td>');
+            let goalsA = $('<td></td>');
             goalsA.val(data.teams[i].goalsAgainst);
             goalsA.html(data.teams[i].goalsAgainst);
 
             row.append(goalsA);
 
-            var goalsD = $('<td></td>');
+            let goalsD = $('<td></td>');
             goalsD.val(data.teams[i].goalsDifference);
             goalsD.html(data.teams[i].goalsDifference);
 
             row.append(goalsD);
 
-            var totalPoints = $('<td></td>');
+            let totalPoints = $('<td></td>');
             totalPoints.val(data.teams[i].total);
             totalPoints.html(data.teams[i].total);
 
