a146ed7093803c7f1acc26549db8ff45f0c3ac3c
Vlad Mocanu <vladmoca90@gmail.com>
2020-06-08 00:09:27 +0100

    Wrote everything as arrow function

--- scripts/JQuery/dropdowns/countries.js
+++ scripts/JQuery/dropdowns/countries.js
@@ -1,31 +1,30 @@
 $(function () {
 
-    function setCountries(xml) {
-        var countriesDropdown = $('#makeDropdown');
-        var countries = xml.find('country');
+    const setCountries = xml => {
+        let countriesDropdown = $('#makeDropdown');
+        let countries = xml.find('country');
 
-        for (var i = 0; i < countries.length; i++) {
-            var country = $(countries[i]);
-            var countryOption = $('<option></option>');
+        for(let i = 0; i < countries.length; i++) {
+            let country = $(countries[i]);
+            let countryOption = $('<option></option>');
 
             countryOption.attr('value', country.attr('code'));
             countryOption.html(country.html());
             countriesDropdown.append(countryOption);
         }
+
         setTotalCount(countries.length);
     }
 
-    function setTotalCount(count) {
-        $('#total-value').html(count);
-    }
+    const setTotalCount = count => $('#total-value').html(count);
 
-    countriesUrl = 'http://sampleapis20180514091454.azurewebsites.net/api/v1/countries';
+    let countriesUrl = 'http://sampleapis20180514091454.azurewebsites.net/api/v1/countries';
 
     $.ajax(countriesUrl, {
         method: 'GET',
         dataType: 'xml',
         success: function (response) {
-            var xml = $(response);
+            let xml = $(response);
 
             $(this).countries = setCountries(xml);
         },
