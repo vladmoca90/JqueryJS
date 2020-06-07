2960805161d0dfaa95aeb881823dc46b61279eb8
Vlad Mocanu <vladmoca90@gmail.com>
2020-06-07 16:00:33 +0100

    Replaced var with let

--- scripts/JQuery/AJAX/echo.js
+++ scripts/JQuery/AJAX/echo.js
@@ -1,18 +1,14 @@
 $(function () {
 
-    var post = $('#echoPost');
-
-    var destination = $('#echoDestination');
-
-    var submit = $('#echoSubmit');
+    // let post = $('#echoPost');
+    // let destination = $('#echoDestination');
+    let submit = $('#echoSubmit');
 
     submit.on('click', function () {
 
-        var content = $('#echoPost').val();
-
-        var json = JSON.parse(content);
-
-        var echoUrl = 'http://sampleapis20180514091454.azurewebsites.net/api/v1/echo';
+        let content = $('#echoPost').val();
+        let json = JSON.parse(content);
+        let echoUrl = 'http://sampleapis20180514091454.azurewebsites.net/api/v1/echo';
 
         $.ajax(echoUrl, {
             method: 'POST',
