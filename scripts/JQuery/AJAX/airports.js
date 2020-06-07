564537fd005b8f9ece3e1625af69636ca54670c6
Vlad Mocanu <vladmoca90@gmail.com>
2020-06-07 15:58:41 +0100

    Replaced var with let

--- scripts/JQuery/AJAX/airports.js
+++ scripts/JQuery/AJAX/airports.js
@@ -1,6 +1,5 @@
 $(function() {
-
-    var airportsUrl = 'https://gist.githubusercontent.com/tdreyno/4278655/raw/7b0762c09b519f40397e4c3e100b097d861f5588/airports.json';
+    let airportsUrl = 'https://gist.githubusercontent.com/tdreyno/4278655/raw/7b0762c09b519f40397e4c3e100b097d861f5588/airports.json';
 
     $.ajax(airportsUrl, {
         method: 'GET',
