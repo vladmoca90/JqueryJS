52bb059ef4913146ddb9637798115528ee96fcbe
Vlad Mocanu <vladmoca90@gmail.com>
2020-06-11 22:29:31 +0100

    Started to do the validation for the authentication

--- scripts/JQuery/authentication.js
+++ scripts/JQuery/authentication.js
@@ -6,6 +6,11 @@ $(function() {
     let check = $('#check');
 
     $('#submit').on('click', function() {
-
+        if(name.length < 3) {
+            throw new Error('');
+        }
+        if(email.length < 3 || email.indexOf('@') == -1) {
+            throw new Error('');
+        }
     });
 }
