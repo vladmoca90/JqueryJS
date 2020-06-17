f516ebabc0b6a1936641c71deaf7d18bd52f20d0
Vlad Mocanu <vladmoca90@gmail.com>
2020-06-17 23:37:36 +0100

    Added .val() where it should be

--- scripts/JQuery/authentication.js
+++ scripts/JQuery/authentication.js
@@ -1,11 +1,11 @@
 $(function() {
-    let name = $('#name');
+    let name = $('#name').val();
     let gender = $('#gender');
-    let email = $('#email');
-    let password = $('#password');
+    let email = $('#email').val();
+    let password = $('#password').val();
     let checkOut = $('#check');
     
-    $('.validation-alerts').hide();
+    $('.validation-alerts p').hide();
 
     $('#submit').on('click', function() {
         if(name.length <= 3) {
