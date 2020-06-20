2e299751a17b0c33a6ed451d9f69c077b51d6cfb
Vlad Mocanu <vladmoca90@gmail.com>
2020-06-20 04:06:29 +0100

    Commented the let variables

--- scripts/JQuery/authentication.js
+++ scripts/JQuery/authentication.js
@@ -1,9 +1,9 @@
 $(function() {
     let name = $('#name').val();
-    let gender = $('#gender');
     let email = $('#email').val();
     let password = $('#password').val();
-    let checkOut = $('#check');
+    // let gender = $('#gender');
+    // let checkOut = $('#check');
 
     $('#submit').on('click', function() {
         if(name.length < 3) {
