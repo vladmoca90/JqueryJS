66ffc6d9836c9fadcb7973dcdf3d0de66ca177cc
Vlad Mocanu <vladmoca90@gmail.com>
2020-06-20 23:31:38 +0100

    Created a proper array of objects

--- scripts/JQuery/people-toggle.js
+++ scripts/JQuery/people-toggle.js
@@ -1,26 +1,25 @@
-const people = {
-    persons: [{
-            name: 'Ana-Maria Magureanu',
-            age: '29',
-        },
-        {
-            name: 'Alexander Swift',
-            age: '25',
-        },
-        {
-            name: 'Caitlin Morgan',
-            age: '30',
-        }, 
-        {
-            name: 'Vladimir Krushchev',
-            age: '35',
-        },
-        {
-            name: 'Leon Williams',
-            age: '29',
-        },
-    ]
-}
+const people = [
+    {
+        name: 'Ana-Maria Magureanu',
+        age: '29',
+    },
+    {
+        name: 'Alexander Swift',
+        age: '25',
+    },
+    {
+        name: 'Caitlin Morgan',
+        age: '30',
+    },
+    {
+        name: 'Vladimir Krushchev',
+        age: '35',
+    },
+    {
+        name: 'Leon Williams',
+        age: '29',
+    },
+]
 
 $(function () {
     let button = $('.dropdown__button');
@@ -31,15 +30,15 @@ $(function () {
     });
 
     const addNames = people => {
-        if(people.persons.length == 0) {
+        if(people.length == 0) {
             throw new Error('The persons list must be given');
         }
 
-        for(let i = 0; i < people.persons.length; i++) {
+        for(let i = 0; i < people.length; i++) {
             let name = $('<li class="dropdown__list-item"></li>');
 
-            name.val(people.persons[i].name);
-            name.html(people.persons[i].name);
+            name.val(people[i].name);
+            name.html(people[i].name);
             list.append(name);
         }
     }
