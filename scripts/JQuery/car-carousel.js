296f5cf53c7834c8a027d448d8590385d3cd785f
Vlad Mocanu <vladmoca90@gmail.com>
2021-02-14 20:58:50 +0000

    Replaced var with let

--- scripts/JQuery/car-carousel.js
+++ scripts/JQuery/car-carousel.js
@@ -1,12 +1,14 @@
 $(function () {
     $('.carousel[data-type="multi"] .item').each(function () {
-        var next = $(this).next();
+        let next = $(this).next();
+
         if (!next.length) {
             next = $(this).siblings(':first');
         }
+
         next.children(':first-child').clone().appendTo($(this));
 
-        for (var i = 0; i < 2; i++) {
+        for (let i = 0; i < 2; i++) {
             next = next.next();
             if (!next.length) {
                 next = $(this).siblings(':first');
