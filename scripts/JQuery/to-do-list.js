598baf37e34297f88a016a1a9526782de7f1950a
Vlad Mocanu <vladmoca90@gmail.com>
2021-04-15 22:53:20 +0100

    Testing

--- scripts/JQuery/to-do-list.js
+++ scripts/JQuery/to-do-list.js
@@ -2,12 +2,12 @@ $(function () {
     let addBtn = $('.add-button');
     let list = $('.list-container');
     let text = $('#field');
+    let close = $('<i class="fa fa-close"></i>');
 
     addBtn.on('click', function () {
         let listItem = $('<li></li>');
         let listContent = text.val();
         let listText = listItem.text(listContent);
-        let close = $('<i class="fa fa-close"></i>');
 
         list.find('ul').append(listText);
         listItem.append(close);
