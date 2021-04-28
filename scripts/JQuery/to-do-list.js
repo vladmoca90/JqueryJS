8bc88e6ddf61f933080b7635b53024528086d0d4
Vlad Mocanu <vladmoca90@gmail.com>
2021-04-28 22:45:25 +0100

    Decommented the code now

--- scripts/JQuery/to-do-list.js
+++ scripts/JQuery/to-do-list.js
@@ -2,15 +2,15 @@ $(function () {
     let addBtn = $('.add-button');
     let list = $('.list-container');
     let text = $('#field');
-    // let close = $('<i class="fa fa-close"></i>');
+    let close = $('<i class="fa fa-close"></i>');
 
     addBtn.on('click', function() {
-        let listItem = $('<li> <i class="fa fa-close"></i></li>');
+        let listItem = $('<li></li>');
         let listContent = text.val();
         let listText = listItem.text(listContent);
 
         list.find('ul').append(listText);
-        // listItem.append(close);
+        listItem.append(close);
 
         if(listContent.trim().length == 0) {
             listItem.remove();
@@ -19,7 +19,7 @@ $(function () {
             return text.val(null);
         }
     });
-    // close.on('click', function() {
-    //     $(this).parent().remove();
-    // });
+    close.on('click', function() {
+        $(this).parent().remove();
+    });
 });
