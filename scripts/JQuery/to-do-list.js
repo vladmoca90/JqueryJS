b1f242d471d36075f4b77bc324209eae8373ad5f
Vlad Mocanu <vladmoca90@gmail.com>
2021-04-28 22:49:04 +0100

    Deleted the commented code

--- scripts/JQuery/to-do-list.js
+++ scripts/JQuery/to-do-list.js
@@ -2,7 +2,6 @@ $(function () {
     let addBtn = $('.add-button');
     let list = $('.list-container');
     let text = $('#field');
-    // let close = $('<i class="fa fa-close"></i>');
 
     addBtn.on('click', function() {
         let listItem = $('<li></li>');
@@ -10,7 +9,6 @@ $(function () {
         let listText = listItem.text(listContent);
 
         list.find('ul').append(listText);
-        // listItem.append(close);
 
         if(listContent.trim().length == 0) {
             listItem.remove();
@@ -19,7 +17,4 @@ $(function () {
             return text.val(null);
         }
     });
-    // close.on('click', function() {
-    //     $(this).parent().remove();
-    // });
 });
