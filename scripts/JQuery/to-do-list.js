3feca605090c92bdb73096828a3ce03e600a6cf5
Vlad Mocanu <vladmoca90@gmail.com>
2021-04-29 21:52:05 +0100

    Added the :first to the jquery

--- scripts/JQuery/to-do-list.js
+++ scripts/JQuery/to-do-list.js
@@ -4,7 +4,7 @@ $(function () {
     let text = $('#field');
 
     addBtn.on('click', function() {
-        let clone = list.find('li.list-item').clone();
+        let clone = list.find('li.list-item:first').clone();
 
         let listContent = text.val();
         let listText = clone.text(listContent);
